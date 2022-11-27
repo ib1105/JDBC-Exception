353c4574b6fe5424e2b0d6c618556fb9ac11d4be
ib1105 <ib1105@naver.com>
2022-11-28 03:02:56 +0900

    v4, v5 Exception commit

--- src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -1,26 +1,26 @@
 package hello.jdbc.repository;
-
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
-
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.NoSuchElementException;
-
 /**
- * JDBC - ConnectionParam
+ * SQLExceptionTranslator 추가
  */
 @Slf4j
-public class MemberRepositoryV3 {
-
+public class MemberRepositoryV4_2 implements MemberRepository {
     private final DataSource dataSource;
-
-    public MemberRepositoryV3(DataSource dataSource) {
+    private final SQLExceptionTranslator exTranslator;
+    public MemberRepositoryV4_2(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
     }
-
-    public Member save(Member member) throws SQLException {
+    @Override
+    public Member save(Member member) {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -32,14 +32,13 @@ public class MemberRepositoryV3 {
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw exTranslator.translate("save", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
-
-    public Member findById(String memberId) throws SQLException {
+    @Override
+    public Member findById(String memberId) {
         String sql = "select * from member where member_id = ?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -55,44 +54,17 @@ public class MemberRepositoryV3 {
                 member.setMoney(rs.getInt("money"));
                 return member;
             } else {
-                throw new NoSuchElementException("member not found memberId=" + memberId);
+                throw new NoSuchElementException("member not found memberId=" +
+                        memberId);
             }
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw exTranslator.translate("findById", sql, e);
         } finally {
             close(con, pstmt, rs);
         }
     }
-
-    public Member findById(Connection con, String memberId) throws SQLException
-    {
-        String sql = "select * from member where member_id = ?";
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, memberId);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                Member member = new Member();
-                member.setMemberId(rs.getString("member_id"));
-                member.setMoney(rs.getInt("money"));
-                return member;
-            } else {
-                throw new NoSuchElementException("member not found memberId=" + memberId);
-            }
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            //connection은 여기서 닫지 않는다.
-            JdbcUtils.closeResultSet(rs);
-            JdbcUtils.closeStatement(pstmt);
-        }
-    }
-
-    public void update(String memberId, int money) throws SQLException {
+    @Override
+    public void update(String memberId, int money) {
         String sql = "update member set money=? where member_id=?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -103,33 +75,13 @@ public class MemberRepositoryV3 {
             pstmt.setString(2, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw exTranslator.translate("update", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
-
-    public void update(Connection con, String memberId, int money) throws SQLException {
-
-        String sql = "update member set money=? where member_id=?";
-        PreparedStatement pstmt = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, money);
-            pstmt.setString(2, memberId);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            //connection은 여기서 닫지 않는다.
-            JdbcUtils.closeStatement(pstmt);
-        }
-    }
-
-    public void delete(String memberId) throws SQLException {
+    @Override
+    public void delete(String memberId) {
         String sql = "delete from member where member_id=?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -139,23 +91,19 @@ public class MemberRepositoryV3 {
             pstmt.setString(1, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw exTranslator.translate("delete", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
-
     private void close(Connection con, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
-        JdbcUtils.closeConnection(con);
+        DataSourceUtils.releaseConnection(con, dataSource);
     }
-
-    private Connection getConnection() throws SQLException {
-        Connection con = dataSource.getConnection();
+    private Connection getConnection() {
+        Connection con = DataSourceUtils.getConnection(dataSource);
         log.info("get connection={} class={}", con, con.getClass());
         return con;
     }
-
 }
